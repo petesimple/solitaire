09815eef1516c476ffc5924d4cc7dbbc325b7533
Pete Lippincott <petesimple@gmail.com>
2026-02-03 20:05:01 -0600

    Update backs.js

--- cards/backs/backs.js
+++ cards/backs/backs.js
@@ -1,4 +1,10 @@
 window.AH_SOLITAIRE_BACKS = [
   "brunswick.png",
-  "dynamo_blue"
+  "Dynamopro.png",
+  "dynamodarkblue.png",
+  "dynamobrown.png",
+  "dynamoblack.png",
+  "dynamopurple.png",
+  "GSGice.png",
+  "GSG.png",
 ];
